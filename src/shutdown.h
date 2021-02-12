fc552cd10aa3993f3ef9cdd4278125b9ececd1fa
Nico V. Urbanczyk <nico@urbansoft.eu>
2021-02-12 21:58:46 +0000

    added cmd line options (e.g. install)

--- src/shutdown.h
+++ src/shutdown.h
@@ -1,6 +1,15 @@
 #ifndef _SHUTDOWN_H_
 #define _SHUTDOWN_H_
 // #############################################################################
+#define MAJOR 0
+#define MINOR 1
+#define PATCH 0
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+#define VERSION STR(MAJOR) "." STR(MINOR) "." STR(PATCH)
+
 #ifndef PIN_NO
 #define PIN_NO Pin05        // default shutdown pin
 #endif
@@ -12,5 +21,10 @@
 #ifndef POLL_DELAY
 #define POLL_DELAY 500ms    // default pin polling interval
 #endif
+
+void ctrl_c(int signo);
+void parseCmdArgs(int argc, char* argv[]);
+void usage();
+void install();
 // #############################################################################
 #endif // _SHUTDOWN_H_
